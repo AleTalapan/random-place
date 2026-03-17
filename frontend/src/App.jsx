d2d01101a644efe19cad566147f73d8b4d3ac9ff
AleTalapan <aletalapan@gmail.com>
2026-03-17 23:53:38 +0200

    Change fetch in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
       setError("")
       setPlace(null)
 
-      const response = await fetch("http://localhost:3001/api/random-place", {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/api/random-place`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
